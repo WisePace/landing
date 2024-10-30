586846d7c7850e10748c4b9a8e4d7e11d85a0944
KostLinux <KostLinux@users.noreply.github.com>
2024-10-30 13:02:26 +0000

    deploy: 19b2b54323745878d97cdc507c0ba0fe6bd2ac9a

--- assets/js/copyright_updater.js
+++ assets/js/copyright_updater.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const currentYear = new Date().getFullYear();
     const copyrightElement = document.getElementById('copyright');
-    copyrightElement.textContent = `© Copyright of Kost Consult 2024 - ${currentYear}`;
+    copyrightElement.textContent = `© Copyright of Kost Consult ${currentYear}`;
 });

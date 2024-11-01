5874d4dc51f7d0e022f67897fb77b9551c4f6561
KostLinux <KostLinux@users.noreply.github.com>
2024-11-01 20:55:28 +0000

    deploy: bacc6e8535d47725562583d3f2343d4009dd615c

--- assets/js/neetocal.js
+++ assets/js/neetocal.js
@@ -1,4 +1,6 @@
-window.neetoCal = window.neetoCal || { embed: function(){(neetoCal.q=neetoCal.q||[]).push(arguments)} };
+window.neetoCal = window.neetoCal || {
+    embed: function() { (neetoCal.q = neetoCal.q || []).push(arguments); }
+};
 
 document.addEventListener('DOMContentLoaded', function() {
     var script = document.createElement('script');
@@ -10,13 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
             id: "0a7f15d6-b793-4403-b7cd-841d178a4a9d",
             organization: "wisepace-team",
             elementSelector: "#open-popup-button",
-            isSidebarAndCoverImgHidden: "true"
-        });
-        neetoCal.embed({
-            type: "elementClick",
-            id: "0a7f15d6-b793-4403-b7cd-841d178a4a9d",
-            organization: "wisepace-team",
-            elementSelector: "#mobile-open-popup-button",
             isSidebarAndCoverImgHidden: "true"
         });
     };

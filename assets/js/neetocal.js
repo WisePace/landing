fa0e2bc9362481688ea4518c82ba6f19bf283e64
KostLinux <christofher.kost@wisepace.org>
2024-11-01 22:53:30 +0200

    fix: remove contact button

--- assets/js/neetocal.js
+++ assets/js/neetocal.js
@@ -12,13 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
             elementSelector: "#open-popup-button",
             isSidebarAndCoverImgHidden: "true"
         });
-        neetoCal.embed({
-            type: "elementClick",
-            id: "0a7f15d6-b793-4403-b7cd-841d178a4a9d",
-            organization: "wisepace-team",
-            elementSelector: "#mobile-open-popup-button",
-            isSidebarAndCoverImgHidden: "true"
-        });
     };
     document.head.appendChild(script);
 });

95ce1dfd48ae7af1d85059f380430d0ca7507d32
KostLinux <christofher.kost@wisepace.org>
2024-11-04 17:04:45 +0200

    fix: footer

--- assets/js/copyright_updater.js
+++ assets/js/copyright_updater.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const currentYear = new Date().getFullYear();
     const copyrightElement = document.getElementById('copyright');
-    copyrightElement.textContent = `© Copyright of WisePath ${currentYear}`;
+    copyrightElement.textContent = `© Copyright of WisePace ${currentYear}`;
 });

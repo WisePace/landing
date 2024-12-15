5764ea8de42b26f5c2be6705928440cc2661444e
KostLinux <KostLinux@users.noreply.github.com>
2024-12-15 21:13:40 +0000

    deploy: df25f271ce7e45305dcb9817bda265882fc15a6a

--- assets/js/mobileMenu.js
+++ assets/js/mobileMenu.js
@@ -1,24 +1,44 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const sectionsToggle = document.getElementById('sectionsToggle');
-    const sectionsDropdown = sectionsToggle.querySelector('.sections-dropdown');
+  const menuBtn = document.getElementById('menu-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
 
-    sectionsToggle.addEventListener('click', (e) => {
-        e.preventDefault();
-        sectionsDropdown.style.display = sectionsDropdown.style.display === 'flex' ? 'none' : 'flex';
-    });
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
 
-    // Handle section link clicks
-    const sectionLinks = sectionsDropdown.querySelectorAll('a');
-    sectionLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-            if (targetElement) {
-                targetElement.scrollIntoView({
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
+  // Adjust scroll position for navigation links
+  const navbarLinks = document.querySelectorAll('nav a, #mobile-menu a');
+  navbarLinks.forEach(link => {
+      link.addEventListener('click', (event) => {
+          const href = link.getAttribute('href');
+          if (href === "#") {
+              // Special case for logo link to scroll to top
+              event.preventDefault();
+              window.scrollTo({
+                  top: 0,
+                  behavior: 'smooth'
+              });
+          } else {
+              event.preventDefault();
+              const targetId = href.substring(1);
+              const targetElement = document.getElementById(targetId);
+
+              if (targetElement) {
+                  const offset = 100; // Adjust this value to create more space on top
+                  const elementPosition = targetElement.getBoundingClientRect().top;
+                  const offsetPosition = elementPosition + window.scrollY - offset;
+
+                  window.scrollTo({
+                      top: offsetPosition,
+                      behavior: 'smooth'
+                  });
+
+                  // Close mobile menu after clicking a link
+                  if (!mobileMenu.classList.contains('hidden')) {
+                      mobileMenu.classList.add('hidden');
+                  }
+              }
+          }
+      });
+  });
 });

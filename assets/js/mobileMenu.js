484c8268189e3e2e9fb9cc74b0dbcb5c69df006b
KostLinux <christofher.kost@wisepace.org>
2024-12-15 22:43:16 +0200

    feat: added smooth scroll and fade in for mobile view, changed contact section

--- assets/js/mobileMenu.js
+++ assets/js/mobileMenu.js
@@ -1,8 +1,44 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const menuBtn = document.getElementById('menu-btn');
-    const mobileMenu = document.getElementById('mobile-menu');
+  const menuBtn = document.getElementById('menu-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
 
-    menuBtn.addEventListener('click', () => {
-      mobileMenu.classList.toggle('hidden');
-    });
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
+
+  // Adjust scroll position for navigation links
+  const navbarLinks = document.querySelectorAll('nav a, #mobile-menu a');
+  navbarLinks.forEach(link => {
+      link.addEventListener('click', (event) => {
+          const href = link.getAttribute('href');
+          if (href === "#") {
+              // Special case for logo link to scroll to top
+              event.preventDefault();
+              window.scrollTo({
+                  top: 0,
+                  behavior: 'smooth'
+              });
+          } else {
+              event.preventDefault();
+              const targetId = href.substring(1);
+              const targetElement = document.getElementById(targetId);
+
+              if (targetElement) {
+                  const offset = 100; // Adjust this value to create more space on top
+                  const elementPosition = targetElement.getBoundingClientRect().top;
+                  const offsetPosition = elementPosition + window.scrollY - offset;
+
+                  window.scrollTo({
+                      top: offsetPosition,
+                      behavior: 'smooth'
+                  });
+
+                  // Close mobile menu after clicking a link
+                  if (!mobileMenu.classList.contains('hidden')) {
+                      mobileMenu.classList.add('hidden');
+                  }
+              }
+          }
+      });
+  });
 });

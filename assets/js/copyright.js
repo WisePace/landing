9cb257066b3b0e1f41dd99fb5cbd49db067684ac
KostLinux <KostLinux@users.noreply.github.com>
2024-12-16 13:40:24 +0000

    deploy: 689d907c076c7326703e26996583bcb2ae34ba16

--- assets/js/copyright.js
+++
@@ -1,6 +0,0 @@
-// Add DOM
-document.addEventListener('DOMContentLoaded', function() {
-    // Update copyright year automatically
-    const year = new Date().getFullYear();
-    document.getElementById("copyright").innerHTML = `&copy; ${year} Wisepace. All rights reserved.`;
-});
